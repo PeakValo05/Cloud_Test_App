0021eeb87327287f2de53e424330136e4634ade0
PeakValo05 <jwiser1@my.gcu.edu>
2026-09-11 15:08:33 -0600

    Inital commit

--- cloudtestapp/src/main/java/com/jackson/cloudtestapp/controllers/TaskController.java
+++ cloudtestapp/src/main/java/com/jackson/cloudtestapp/controllers/TaskController.java
@@ -45,6 +45,11 @@ public String viewTasks(Model model) {
     return "tasks";
 }
 
+
+
+
+
+
     // Display add-task form
     @GetMapping("/tasks/add")
     public String showAddTaskForm(Model model) {
@@ -63,6 +68,11 @@ public String viewTasks(Model model) {
         return "add-task";
     }
 
+
+
+
+
+
     // Save a new task
     @PostMapping("/tasks/save")
     public String saveTask(@ModelAttribute("task") Task task, RedirectAttributes redirectAttribute) {
@@ -75,6 +85,10 @@ public String viewTasks(Model model) {
         return "redirect:/tasks";
     }
 
+
+
+
+
     // Display edit-task form
 @GetMapping("/tasks/edit/{id}")
 public String showEditTaskForm(@PathVariable("id") Long id, Model model, RedirectAttributes redirectAttribute) {
@@ -82,21 +96,35 @@ public String showEditTaskForm(@PathVariable("id") Long id, Model model, Redirec
 
     if (task == null) {
         logger.warn("Task not found with ID: {}", id);
+        
         redirectAttribute.addFlashAttribute("errorMessage", "Task not found!");
+
         return "redirect:/tasks";
     }
 
     model.addAttribute("task", task);
     logger.info("Displaying edit-task form for task with ID: {}", id);
+
     logger.info("Exiting TaskController.showEditTaskForm");
+
     return "edit-tasks";
 }
+
+
+
+
+
     // Delete a task
 @GetMapping("/tasks/delete/{id}")
 public String deleteTask(@PathVariable("id") Long id, RedirectAttributes redirectAttribute) {
     taskService.deleteTask(id);
     logger.info("Task deleted with ID: {}", id);
+
     redirectAttribute.addFlashAttribute("successMessage", "Task deleted successfully!");
+
+    logger.info("Redirecting to view all tasks after deletion");
+
+    logger.info("Exiting TaskController.deleteTask");
     return "redirect:/tasks";
 }
 }
